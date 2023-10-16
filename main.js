3c3622c17eae0a67b9bfeb44fe77ab49bd6d784d
abaduna <64230830+abaduna@users.noreply.github.com>
2023-10-16 17:47:53 -0300

    terminado

--- main.js
+++ main.js
@@ -4,19 +4,24 @@ const agregarJugada=(valor,jugador)=>{
 
     if (jugador === "jugador_1") {
         jugada_n_1 = valor
+       
+        
 
     } else if (jugador === "jugador_2") {
         jugador_n_2 = valor
         
+        
     }
-
+    
 
     ganar()
 }
 const borrar=()=>{
 
     if (jugada_n_1 !="") {
+        
         jugada_n_1 = ""
+        
     }
     if (jugador_n_2 !="") {
         jugador_n_2 = ""
@@ -26,26 +31,26 @@ const borrar=()=>{
 const ganar =()=>{
 
     if (jugada_n_1 ==="tijera") {
-     if (jugador_n_2==="tijera")  { mostrarResultado("empate")  }   
-     if (jugador_n_2==="rock") {mostrarResultado("gano el jugador n2") }   
-     if (jugador_n_2==="paper") {mostrarResultado("gano el jugador n1") }   
+     if (jugador_n_2==="tijera")  { mostrarResultado("Empate")  }   
+     if (jugador_n_2==="rock") {mostrarResultado("Gano el jugador N 2") }   
+     if (jugador_n_2==="paper") {mostrarResultado("Gano el jugador N 1") }   
      }
     if (jugada_n_1 ==="paper") {
-        if (jugador_n_2==="tijera") {mostrarResultado("gano el jugador n2") }   
-        if (jugador_n_2==="rock") {mostrarResultado("gano el jugador n1") }   
-        if (jugador_n_2==="paper") {mostrarResultado("empate") }   
+        if (jugador_n_2==="tijera") {mostrarResultado("Gano el jugador N 2") }   
+        if (jugador_n_2==="rock") {mostrarResultado("Gano el jugador N 1") }   
+        if (jugador_n_2==="paper") {mostrarResultado("Empate") }   
        }
        if (jugada_n_1 ==="rock") {
-        if (jugador_n_2==="tijera") {mostrarResultado("gano el jugador n1") }   
-        if (jugador_n_2==="rock") {mostrarResultado("empate") }   
-        if (jugador_n_2==="paper") {mostrarResultado("gano el jugador n2") }   
+        if (jugador_n_2==="tijera") {mostrarResultado("Gano el jugador N 1") }   
+        if (jugador_n_2==="rock") {mostrarResultado("Empate") }   
+        if (jugador_n_2==="paper") {mostrarResultado("Gano el jugador N 2") }   
        }
-
+     
 
     }
 const mostrarResultado =(resultado)=>{
-    console.log(resultado)
-    mostrarResultadodiv = document.getElementById("mostrarResultado")
+    
+    // document.getElementById(`id-n2-${jugador_n_2}`).classList.add('selecionada-n-2');
     mostrarResultadodiv.innerHTML = ` <h2 class="resultado">${resultado}</h2>`
     borrar()
    
