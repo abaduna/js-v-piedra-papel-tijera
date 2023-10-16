9a436dd32890f44bb04667dcbfa56b1b79aac876
abaduna <64230830+abaduna@users.noreply.github.com>
2023-10-16 17:04:31 -0300

    terminado todas las funcionalidades

--- main.js
+++ main.js
@@ -10,14 +10,45 @@ const agregarJugada=(valor,jugador)=>{
         
     }
 
-    console.log(`el jugada del jugador n1 es ${jugada_n_1}`) //aca da lo que tien que dar
-    console.log(`el jugada del jugador n2 es ${jugador_n_2}`)//aca da lo que tien que dar
+
     ganar()
 }
-const ganar =(jugada_n_1,jugador_n_2)=>{
-    console.log(`el jugada del jugador n1 es ${jugada_n_1}`) //aca da undefined
-    console.log(`el jugada del jugador n2 es ${jugador_n_2}`)//aca da undefined
+const borrar=()=>{
 
+    if (jugada_n_1 !="") {
+        jugada_n_1 = ""
+    }
+    if (jugador_n_2 !="") {
+        jugador_n_2 = ""
+    }
+
+}
+const ganar =()=>{
+
+    if (jugada_n_1 ==="tijera") {
+     if (jugador_n_2==="tijera")  { mostrarResultado("empate")  }   
+     if (jugador_n_2==="rock") {mostrarResultado("gano el jugador n2") }   
+     if (jugador_n_2==="paper") {mostrarResultado("gano el jugador n1") }   
+     }
+    if (jugada_n_1 ==="paper") {
+        if (jugador_n_2==="tijera") {mostrarResultado("gano el jugador n2") }   
+        if (jugador_n_2==="rock") {mostrarResultado("gano el jugador n1") }   
+        if (jugador_n_2==="paper") {mostrarResultado("empate") }   
+       }
+       if (jugada_n_1 ==="rock") {
+        if (jugador_n_2==="tijera") {mostrarResultado("gano el jugador n1") }   
+        if (jugador_n_2==="rock") {mostrarResultado("empate") }   
+        if (jugador_n_2==="paper") {mostrarResultado("gano el jugador n2") }   
+       }
+
+
+    }
+const mostrarResultado =(resultado)=>{
+    console.log(resultado)
+    mostrarResultadodiv = document.getElementById("mostrarResultado")
+    mostrarResultadodiv.innerHTML = ` <h2 class="resultado">${resultado}</h2>`
+    borrar()
+   
 }
 const start=()=>{
     document.getElementById("tijera").addEventListener("click",()=>agregarJugada("tijera","jugador_1"))
